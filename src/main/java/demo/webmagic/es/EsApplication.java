fa871965aabc7f4a10f3d7a1518591b705f2efec
linzhiqiang <496057338@qq.com>
2022-06-17 17:43:32 +0800

    初次提交

--- src/main/java/demo/webmagic/es/EsApplication.java
+++ src/main/java/demo/webmagic/es/EsApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EsApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(EsApplication.class, args);
     }

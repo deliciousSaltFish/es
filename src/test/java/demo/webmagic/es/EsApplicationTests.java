de608552b0d300ebb73fdf3aaaae8fe8a75ede7c
linzhiqiang <496057338@qq.com>
2022-06-17 17:09:34 +0800

    初次提交

---
+++ src/test/java/demo/webmagic/es/EsApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.webmagic.es;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
